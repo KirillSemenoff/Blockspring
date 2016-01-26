ca9b807527b728ee4276e33b264c1cc59ce8f7cc
dexterdl86@gmail.com <P@ssw0rd>
2016-01-26 10:17:16 +0200

    first commit on Blockspring automation

---
+++ src/test/java/db_queries/Action.java
@@ -0,0 +1,14 @@
+package db_queries;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by ksemenov on 1/25/2016.
+ */
+public class Action {
+    @When("^we run a block with default value, \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void weRunABlockWithDefaultValueAnd(String arg0, String arg1) throws Throwable {
+
+    }
+}
